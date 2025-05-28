02d48cb20ee9c4a2a629081967b29ea297acd9bf
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-28 16:27:54 +0800

    Created shared Errorcode configurations

---
+++ shared/src/main/java/com/jphilips/onlineshop/shared/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package com.jphilips.onlineshop.shared.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ErrorCode {
+    // === Standard HTTP Errors ===
+    INTERNAL_SERVER_ERROR(500, "ERROR_INTERNAL_SERVER"),
+    UNAUTHORIZED(401, "ERROR_UNAUTHORIZED"),
+    NOT_FOUND(404, "ERROR_NOT_FOUND"),
+    BAD_REQUEST(400, "ERROR_BAD_REQUEST"),
+
+    // === Custom Application Errors ===
+    USER_NOT_FOUND(400, "ERROR_USER_NOT_FOUND"),
+    INVALID_INPUT(400, "ERROR_INVALID_INPUT");
+
+    private final int status;
+    private final String messageCode;
+
+}
