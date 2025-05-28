af6644b7efabba75d052f9ef86c14456e30bca8b
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-28 15:31:40 +0800

    Created Exception response handler

---
+++ shared/src/main/java/com/jphilips/onlineshop/shared/dto/ExceptionResponseDTO.java
@@ -0,0 +1,18 @@
+package com.jphilips.onlineshop.shared.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Builder
+public record ExceptionResponseDTO(
+        LocalDateTime timestamp,
+        int status,
+        String error,
+        String message,
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        Map<String, String> fieldErrors,
+        String path) {
+}
