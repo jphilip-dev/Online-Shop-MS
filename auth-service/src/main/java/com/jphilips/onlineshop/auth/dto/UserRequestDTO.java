6c1fe21eee2558abc7a614822062e328c4d3a0c7
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-28 15:32:35 +0800

    Created User request DTO with Validation groupings and User response DTO (simple)

---
+++ auth-service/src/main/java/com/jphilips/onlineshop/auth/dto/UserRequestDTO.java
@@ -0,0 +1,30 @@
+package com.jphilips.onlineshop.auth.dto;
+
+import com.jphilips.onlineshop.auth.validator.UniqueEmail;
+import com.jphilips.onlineshop.shared.validator.groups.OnCreate;
+import com.jphilips.onlineshop.shared.validator.groups.OnUpdate;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserRequestDTO {
+
+    @Email(groups = {OnCreate.class, OnUpdate.class})
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
+    @UniqueEmail(groups = OnCreate.class, message = "Email is already in use")
+    private String email;
+
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
+    private String name;
+
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
+    @Size(min = 6, groups = {OnCreate.class, OnUpdate.class}, message = "ERROR_MINIMUM_LENGTH_SIX_CHARS")
+    private String password;
+
+}
