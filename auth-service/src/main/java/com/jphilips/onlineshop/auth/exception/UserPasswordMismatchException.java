0db7a388779419f4707a7fb4b5b6d6b571303e0d
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-28 17:13:33 +0800

    Created custom exceptions

---
+++ auth-service/src/main/java/com/jphilips/onlineshop/auth/exception/UserPasswordMismatchException.java
@@ -0,0 +1,13 @@
+package com.jphilips.onlineshop.auth.exception;
+
+import com.jphilips.onlineshop.shared.exception.ErrorCode;
+
+public class UserPasswordMismatchException extends UserException {
+    public UserPasswordMismatchException(ErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+
+    public UserPasswordMismatchException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
