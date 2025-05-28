20e299884e05b5838831df0728e453cc7b696b64
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-28 17:48:16 +0800

    Refactor validation messages

--- auth-service/src/main/java/com/jphilips/onlineshop/auth/dto/LoginRequestDTO.java
+++ auth-service/src/main/java/com/jphilips/onlineshop/auth/dto/LoginRequestDTO.java
@@ -1,7 +1,5 @@
 package com.jphilips.onlineshop.auth.dto;
 
-import com.jphilips.onlineshop.shared.validator.groups.OnCreate;
-import com.jphilips.onlineshop.shared.validator.groups.OnUpdate;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -10,6 +8,7 @@ public record LoginRequestDTO(
         @Email(message = "{email.invalid}")
         @NotBlank(message = "{email.blank}")
         String email,
+
         @NotBlank(message = "{password.blank}")
         @Size(min = 6, message = "{password.tooShort}")
         String password) {
--- auth-service/src/main/java/com/jphilips/onlineshop/auth/dto/UserRequestDTO.java
+++ auth-service/src/main/java/com/jphilips/onlineshop/auth/dto/UserRequestDTO.java
@@ -15,16 +15,16 @@ import lombok.NoArgsConstructor;
 @Data
 public class UserRequestDTO {
 
-    @Email(groups = {OnCreate.class, OnUpdate.class})
-    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
-    @UniqueEmail(groups = OnCreate.class, message = "Email is already in use")
+    @Email(groups = {OnCreate.class, OnUpdate.class}, message = "{email.invalid}")
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "{email.blank}")
+    @UniqueEmail(groups = OnCreate.class, message = "{email.taken}")
     private String email;
 
-    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "{name.blank}")
     private String name;
 
-    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
-    @Size(min = 6, groups = {OnCreate.class, OnUpdate.class}, message = "ERROR_MINIMUM_LENGTH_SIX_CHARS")
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class},message = "{password.blank}")
+    @Size(min = 6, groups = {OnCreate.class, OnUpdate.class}, message = "{password.tooShort}")
     private String password;
 
 }
