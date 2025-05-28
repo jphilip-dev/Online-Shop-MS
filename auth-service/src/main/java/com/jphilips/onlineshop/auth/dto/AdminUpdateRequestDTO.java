f84a0303f80c0d06cd6ba50a32a4f9d371580248
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-05-28 18:28:51 +0800

    Add Admin service to update all fields of the user

---
+++ auth-service/src/main/java/com/jphilips/onlineshop/auth/dto/AdminUpdateRequestDTO.java
@@ -0,0 +1,37 @@
+package com.jphilips.onlineshop.auth.dto;
+
+import com.jphilips.onlineshop.auth.validator.UniqueEmail;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AdminUpdateRequestDTO {
+
+    @NotNull(message = "{required.notnull}")
+    private Long id;
+
+    @Email( message = "{email.invalid}")
+    @NotBlank( message = "{email.blank}")
+    @UniqueEmail( message = "{email.taken}")
+    private String email;
+
+    @NotBlank(message = "{name.blank}")
+    private String name;
+
+    @NotBlank(message = "{password.blank}")
+    @Size(message = "{password.tooShort}")
+    private String password;
+
+    @NotNull(message = "{required.notnull}")
+    private Boolean isActive;
+
+    @NotNull(message = "{required.notnull}")
+    private Boolean isAdmin;
+}
