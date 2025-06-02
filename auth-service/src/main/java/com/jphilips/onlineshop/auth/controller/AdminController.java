9aebd1b4d9a6652b0148a3b7c7a4724bccc534c0
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-02 20:23:59 +0800

    Added Admin endpoint to update Users

--- auth-service/src/main/java/com/jphilips/onlineshop/auth/controller/AdminController.java
+++ auth-service/src/main/java/com/jphilips/onlineshop/auth/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.jphilips.onlineshop.auth.controller;
 
 
+import com.jphilips.onlineshop.auth.dto.AdminUpdateRequestDTO;
 import com.jphilips.onlineshop.auth.dto.UserResponseDTO;
 import com.jphilips.onlineshop.auth.service.admin.GetUsersServiceHandler;
+import com.jphilips.onlineshop.auth.service.admin.UpdateUserAdminService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,19 +18,25 @@ import java.util.List;
 public class AdminController {
 
     private final GetUsersServiceHandler getUserServiceHandler ;
+    private final UpdateUserAdminService updateUserAdminService;
 
     @GetMapping("/all-users")
     public ResponseEntity<List<UserResponseDTO>> getAllUsers(){
         return ResponseEntity.status(HttpStatus.OK).body(getUserServiceHandler.getAllUsers());
     }
-    @GetMapping("/user")
+    @GetMapping("/users")
     public ResponseEntity<UserResponseDTO> getUserByEmail(@RequestParam String email) {
         return ResponseEntity.status(HttpStatus.OK).body(getUserServiceHandler.getUserByEmail(email));
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public ResponseEntity<UserResponseDTO>  getUserById(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(getUserServiceHandler.getUserById(id));
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<UserResponseDTO>  updateUserById(@RequestBody AdminUpdateRequestDTO adminUpdateRequestDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(updateUserAdminService.execute(adminUpdateRequestDTO));
     }
 
 }
