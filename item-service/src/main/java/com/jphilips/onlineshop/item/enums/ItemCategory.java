b675afe180ec9792763d39be928ce8281a6cce49
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-05 15:51:17 +0800

    Add more categories

--- item-service/src/main/java/com/jphilips/onlineshop/item/enums/ItemCategory.java
+++ item-service/src/main/java/com/jphilips/onlineshop/item/enums/ItemCategory.java
@@ -3,5 +3,20 @@ package com.jphilips.onlineshop.item.enums;
 public enum ItemCategory {
     CLOTHING,
     FOOD,
+    ELECTRONICS,
+    HOME_APPLIANCES,
+    FURNITURE,
+    TOYS,
+    BOOKS,
+    BEAUTY,
+    SPORTS,
+    AUTOMOTIVE,
+    STATIONERY,
+    PET_SUPPLIES,
+    HEALTH,
+    GARDEN,
+    JEWELRY,
+    FOOTWEAR,
+    ACCESSORIES,
     OTHERS
 }
