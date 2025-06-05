0475b9cc27cdd4f31f143b7b6b4c9f184423d95e
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-05 15:39:57 +0800

    Add Item entity with Enum categories

---
+++ item-service/src/main/java/com/jphilips/onlineshop/item/entity/Item.java
@@ -0,0 +1,52 @@
+package com.jphilips.onlineshop.item.entity;
+
+import com.jphilips.onlineshop.item.enums.ItemCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "items")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String sku;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ItemCategory category;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int stocks;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false, columnDefinition = "Boolean")
+    private boolean isActive;
+
+    private String brand;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
---
+++ item-service/src/main/java/com/jphilips/onlineshop/item/enums/ItemCategory.java
@@ -0,0 +1,7 @@
+package com.jphilips.onlineshop.item.enums;
+
+public enum ItemCategory {
+    CLOTHING,
+    FOOD,
+    OTHERS
+}
