a9272b496d9d3535cf9592bb752e69a4d7a58427
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-05 18:42:17 +0800

    Add the Shared dependency to help with Errorcode and Exceptions

---
+++ item-service/src/main/java/com/jphilips/onlineshop/item/exception/ItemExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.jphilips.onlineshop.item.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ItemExceptionHandler {
+}
