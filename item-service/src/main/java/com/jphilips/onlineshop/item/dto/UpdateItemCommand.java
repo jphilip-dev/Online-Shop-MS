bfa8b43a0666e8a319a903200e9baafa0416fb91
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-06 12:56:36 +0800

    Refactor to make fields readable and create command dto for UpdateItemCommand

--- item-service/src/main/java/com/jphilips/onlineshop/item/dto/UpdateItemCommand.java
+++ item-service/src/main/java/com/jphilips/onlineshop/item/dto/UpdateItemCommand.java
@@ -2,7 +2,8 @@ package com.jphilips.onlineshop.item.dto;
 
 import com.jphilips.onlineshop.shared.dto.UserDetailsDTO;
 
-public record CreateItemCommand(
+public record UpdateItemCommand(
+        Long itemId,
         UserDetailsDTO userDetailsDTO,
         ItemRequestDTO itemRequestDTO) {
 }
