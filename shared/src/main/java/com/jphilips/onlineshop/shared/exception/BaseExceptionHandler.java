b751f3f6ea947e4290e63199199b4b6dd74d0137
jphilip-dev <john.p.s.de.roxas+git@gmail.com>
2025-06-07 10:01:41 +0800

    Add Abstract BaseExceptionHandler

---
+++ shared/src/main/java/com/jphilips/onlineshop/shared/exception/BaseExceptionHandler.java
@@ -0,0 +1,71 @@
+package com.jphilips.onlineshop.shared.exception;
+
+import com.jphilips.onlineshop.shared.dto.ExceptionResponseDTO;
+import com.jphilips.onlineshop.shared.exception.custom.AppException;
+import com.jphilips.onlineshop.shared.exception.custom.BaseException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public abstract class BaseExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request){
+        return buildResponseFrom(
+                new AppException(ErrorCode.BAD_REQUEST),
+                ex.getBindingResult().getFieldErrors(),
+                request
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponseDTO> handleOtherExceptions(Exception ex, WebRequest request){
+
+        log.error("Unhandled error: {}", ex.getMessage(), ex);
+
+        return buildResponseFrom(
+                new AppException(ErrorCode.INTERNAL_SERVER_ERROR),
+                null,
+                request
+        );
+    }
+
+    protected ResponseEntity<ExceptionResponseDTO> buildResponseFrom(
+            BaseException ex,
+            List<FieldError> fieldErrorsList,
+            WebRequest request) {
+
+        HttpStatus status = ex.getErrorCode().getStatus();
+
+        Map<String, String> fieldErrors = null; // null is handled by JsonInclude
+
+        if (fieldErrorsList != null){
+            fieldErrors = new HashMap<>();
+            for (FieldError fieldError : fieldErrorsList){
+                fieldErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+        }
+
+        var response = new ExceptionResponseDTO(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                ex.getErrorCode().getMessageCode(),
+                ex.getMessage() == null || ex.getMessage().isBlank() ? null : ex.getMessage(),
+                fieldErrors,
+                request.getDescription(false).replace("uri=", "")
+        );
+
+        return ResponseEntity.status(status).body(response);
+    }
+}
